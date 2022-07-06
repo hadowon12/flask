e3c0fd105dea29a15169163da7f31cdad67c7f65
hadowon12 <hadowon12@naver.com>
2022-07-06 20:49:07 +0900

    test

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/add_two_nums', methods=['POST'])  # POST 데이터를 추가.
-def add_two_nums():
-    data = request.get_json()  # 클라이언트로부터 json형식으로 데이터를 받는다.
-    return data
-
-if __name__ == "__main__":   # flask 마지막에 들어가는 기본
-    app.run()
+@app.route("/hello", methods=['GET'])
+def hello():
+  return "hello world"
