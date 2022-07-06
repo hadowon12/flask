ef49e0c22d036110b7051754aae13c5f51396b3c
hadowon12 <hadowon12@naver.com>
2022-07-06 21:33:37 +0900

    test

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/hello", methods=['GET'])
-def hello():
-  return "hello world"
+
+@app.route('/userLogin', methods=['POST'])
+def userLogin():
+    user = request.get_json()  # json 데이터를 받아옴
+    return jsonify(user)  # 받아온 데이터를 다시 전송
+
+
+@app.route('/environments/<language>')
+def environments(language):
+    return jsonify({"language": language})
+
+
+if __name__ == "__main__":
+    app.run()
